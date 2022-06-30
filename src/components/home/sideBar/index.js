46876ae9e6bc4c67696fdd0a67a463b29e65df34
sreenath707 <sreenathsharma707@gmail.com>
2022-06-30 21:15:08 +0530

    conflict bugs resolved

---
+++ src/components/home/sideBar/index.js
@@ -0,0 +1,58 @@
+import HomeIcon from "@mui/icons-material/Home";
+import SearchIcon from "@mui/icons-material/Search";
+import LibraryBooksIcon from "@mui/icons-material/LibraryBooks";
+import AddBoxIcon from "@mui/icons-material/AddBox";
+import FavoriteIcon from "@mui/icons-material/Favorite";
+import { useState } from "react";
+
+function SideBar() {
+  const [playlists, setPlaylists] = useState([
+    "telugu",
+    "twenty one pilots",
+    "Today's Top Hits",
+    "one direction",
+    "Eminem",
+    "Nial Horam",
+  ]);
+
+  return (
+    <div className="side-menu">
+      <img
+        className="main-logo"
+        src="https://storage.googleapis.com/pr-newsroom-wp/1/2018/11/Spotify_Logo_CMYK_White.png"
+        alt="logo"
+      ></img>
+      <div className="side-bar-top">
+        <div className="side-bar-tags">
+          <div className="side-bar-tag">
+            <HomeIcon />
+            <div className="side-bar-tag-name">Home</div>
+          </div>
+          <div className="side-bar-tag">
+            <SearchIcon />
+            <div className="side-bar-tag-name">Search</div>
+          </div>
+          <div className="side-bar-tag">
+            <LibraryBooksIcon />
+            <div className="side-bar-tag-name">Your Library</div>
+          </div>
+          <div className="side-bar-tag">
+            <AddBoxIcon />
+            <div className="side-bar-tag-name">Create Playlist</div>
+          </div>
+          <div className="side-bar-tag">
+            <FavoriteIcon />
+            <div className="side-bar-tag-name">Liked Songs</div>
+          </div>
+          <div className="bottom-home"></div>
+        </div>
+      </div>
+      <div className="side-bar-bottom">
+        {playlists.map((playlist) => (
+          <div className="side-bar-playlist">{playlist}</div>
+        ))}
+      </div>
+    </div>
+  );
+}
+export default SideBar;
